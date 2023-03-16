2cfd4524671769e768e9b68798da69315cbb38f5
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-03-16 14:49:47 -0700

    new commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - entry point
+*return: always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	*  return main is 0 (success)
+	*/
+	return (0);
+}
