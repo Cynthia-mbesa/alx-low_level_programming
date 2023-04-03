f2e2b9400c4f7f7732cd620b3d23092bc79ee7df
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-04-03 15:48:45 -0700

    new commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,11 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s == c || c == '\0')
+		if (*s == c && c != '\0')
 		{
 			return (s);
 		}
 		s++;
 	}
-	return ('\0');
+	return (NULL);
 }
