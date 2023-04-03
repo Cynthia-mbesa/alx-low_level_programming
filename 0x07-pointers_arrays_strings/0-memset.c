56d52ad77c31c4055d97783a4e8a50250d2ff039
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-04-03 13:55:11 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_memset - fills memory with a constant byte
+ *@s: pointer to the memory
+ *@b: character to be filled in s
+ *@n: number of bytes
+ *Return: returns memory location s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
