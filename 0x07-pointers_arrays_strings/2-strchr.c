ba1e38641c5e17725af115c7341132e3224d6f34
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-04-03 15:12:26 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strchr - return location of c
+ *@s: pointer to a string
+ *@c: character to be found
+ *
+ *Return: s or null
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
