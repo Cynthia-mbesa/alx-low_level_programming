6d77cef9f7998e811401f729a2fd75dc8421a6e2
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-04-05 11:13:41 -0700

    new commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_pow_recursion - x raised to the power of y
+ *@x: the base value
+ *@y: the power
+ *Return: -1(x^(y<0), 1(x^0), x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, (y - 1)));
+	}
+}
