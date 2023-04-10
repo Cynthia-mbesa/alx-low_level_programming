43571be270a207b6f321bf8da3d71e7ef19366c6
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-04-10 15:51:51 -0700

    learning argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0(success)
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
