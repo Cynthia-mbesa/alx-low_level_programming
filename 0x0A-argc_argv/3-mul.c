9cd1d577698c1f82d3f62afa732a8c2c3cf38465
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-04-10 17:22:13 -0700

    learning argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry point
+ *@argc: argument counter
+ *@argv: argument vector
+ *
+ *Return: 0(success) 1(failed)
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int mul = 0;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	mul = x * y;
+	printf("%d\n", mul);
+	return (0);
+}
