99649bdf9e63b0fcb581e86b9e25a4819fee55b6
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-04-17 07:57:46 -0700

    structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - structure description
+ *@name: name of dog
+ *@owner: owner of dog
+ *@age: age of dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
