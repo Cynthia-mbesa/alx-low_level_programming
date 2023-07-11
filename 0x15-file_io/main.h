ac6e6701a9c0bc104680f8383ff96c9523f73e9c
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-07-11 04:56:33 +0300

    file I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,9 +2,13 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
